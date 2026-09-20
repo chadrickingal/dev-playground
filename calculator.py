b7f8ef4b3cce22c9a774ff3469429e57750784ff
Chadrick <ichadrickryan@gmail.com>
2026-09-20 23:57:07 +0800

    feat: add modulu operation

--- calculator.py
+++ calculator.py
@@ -13,3 +13,6 @@ def divide(firstNUmber, secondNumber):
     if firstNUmber == 0: 
         print("Cannot devide by zero (0)")
     return firstNUmber / secondNumber
+
+def modulu(firstNumber, secondNumber):
+    return firstNumber % secondNumber
